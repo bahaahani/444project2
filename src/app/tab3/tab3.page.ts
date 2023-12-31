419acf3649125a1991742de4f1d0274ca7967548
PG38 <alshabah2000@windowslive.com>
2023-12-31 12:01:21 +0300

    .

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { DataService } from '../data.service';
-//import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
 import { AnimationController, IonItem } from '@ionic/angular';
 import { Animation } from '@ionic/core';
-// tab3.page.ts
+
 
 @Component({
   selector: 'app-tab3',
@@ -19,7 +19,7 @@ export class Tab3Page implements OnInit {
   ngOnInit() {
     this.dataService.getPrintRequests().subscribe(data => {
       this.list1 = data;
-      // Assume all data goes into list1, or split as needed
+    
     });
   }
 }
