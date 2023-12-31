d7f19545f361454beec4f287f5857f4b0fe383ca
Bahaa Hani <blainefire00@gmail.com>
2023-12-31 11:47:45 +0300

    .

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -3,6 +3,7 @@ import { DataService } from '../data.service';
 //import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { AnimationController, IonItem } from '@ionic/angular';
 import { Animation } from '@ionic/core';
+// tab3.page.ts
 
 @Component({
   selector: 'app-tab3',
@@ -13,52 +14,12 @@ export class Tab3Page implements OnInit {
   list1: any[] = [];
   list2: any[] = [];
 
-  //@ViewChildren(IonItem) items: QueryList<IonItem>;
-
-  constructor(private dataService: DataService, private animationCtrl: AnimationController) {}
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
     this.dataService.getPrintRequests().subscribe(data => {
       this.list1 = data;
-      // Initialize list2 if needed or do additional processing
+      // Assume all data goes into list1, or split as needed
     });
   }
-
-  /*drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    }
-  }*/
-
-  print() {
-   // this.animateItems('forward');
-  }
-
-  reversePrint() {
-    //this.animateItems('reverse');
-  }
-
-  // animateItems(direction: 'forward' | 'reverse') {
-  //   const itemsArray = this.items.toArray();
-  //   if (direction === 'reverse') itemsArray.reverse();
-
-  //   itemsArray.forEach((item, index) => {
-  //     const animation = this.animationCtrl
-  //       .create()
-  //       .addElement(item.el)
-  //       .duration(300)
-  //       .delay(index * 100)
-  //       .fromTo('opacity', '1', '0.5')
-  //       .fromTo('transform', 'translateX(0)', 'translateX(100px)');
-
-  //     animation.play();
-  //   });
-  // }
 }
